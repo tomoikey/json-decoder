e3ef22f1765f6f2a37db5a9aa772ae67d9113b09
tomoikey <55743826+tomoikey@users.noreply.github.com>
2023-01-27 11:16:37 +0900

    aaa

--- src/json/json_parser.rs
+++ src/json/json_parser.rs
@@ -10,9 +10,75 @@ pub trait JsonDecoder<T> {
     }
 }
 
+impl JsonDecoder<i8> for i8 {
+    fn parser(result: &DecodeResult) -> i8 {
+        result.as_i8()
+    }
+}
+
+impl JsonDecoder<i16> for i16 {
+    fn parser(result: &DecodeResult) -> i16 {
+        result.as_i16()
+    }
+}
+
+impl JsonDecoder<i32> for i32 {
+    fn parser(result: &DecodeResult) -> i32 {
+        result.as_i32()
+    }
+}
+
+impl JsonDecoder<i64> for i64 {
+    fn parser(result: &DecodeResult) -> i64 {
+        result.as_i64()
+    }
+}
+
+impl JsonDecoder<i128> for i128 {
+    fn parser(result: &DecodeResult) -> i128 {
+        result.as_i128()
+    }
+}
+
 impl JsonDecoder<isize> for isize {
     fn parser(result: &DecodeResult) -> isize {
         result.as_isize()
+    }
+}
+
+impl JsonDecoder<u8> for u8 {
+    fn parser(result: &DecodeResult) -> u8 {
+        result.as_u8()
+    }
+}
+
+impl JsonDecoder<u16> for u16 {
+    fn parser(result: &DecodeResult) -> u16 {
+        result.as_u16()
+    }
+}
+
+impl JsonDecoder<u32> for u32 {
+    fn parser(result: &DecodeResult) -> u32 {
+        result.as_u32()
+    }
+}
+
+impl JsonDecoder<u64> for u64 {
+    fn parser(result: &DecodeResult) -> u64 {
+        result.as_u64()
+    }
+}
+
+impl JsonDecoder<u128> for u128 {
+    fn parser(result: &DecodeResult) -> u128 {
+        result.as_u128()
+    }
+}
+
+impl JsonDecoder<usize> for usize {
+    fn parser(result: &DecodeResult) -> usize {
+        result.as_usize()
     }
 }
 
